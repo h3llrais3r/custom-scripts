67b1300a5378959b8b98dcb6c8a455a3ad6cc054
h3llrais3r <pooh_beer_1@hotmail.com>
2017-11-14 20:29:58 +0100

    Add logoffAndRestoreConsoleSessionWithSleep.bat

---
+++ windows/rdp/logoffAndRestoreConsoleSessionWithSleep.bat
@@ -0,0 +1,8 @@
+@rem Logoff from remote desktop and restore the console (sessinID = 1) session and put computer to sleep
+@rem In case you have hibernate enabled on your pc, you'll also need to disable it (see https://winaero.com/blog/how-to-sleep-a-windows-computer-from-the-command-line/)
+@rem A small timeout is added in between to be sure the remote desktop connection is properly ended and console is restored
+@rem Run with administrative rights
+@rem Program location: %windir%\system32
+tscon.exe 1 /dest:console
+timeout 2 /nobreak
+rundll32.exe powrprof.dll,SetSuspendState 0,1,0

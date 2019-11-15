31c54a585b07daa69ecbd7bf56ff6c542ab8c1ae
h3llrais3r <pooh_beer_1@hotmail.com>
2019-11-16 00:29:14 +0100

    Update kodi autohotkey scripts

--- windows/autohotkey/kodi/load_receiver_settings.py
+++ windows/autohotkey/kodi/load_receiver_settings.py
@@ -2,42 +2,42 @@ import json
 import urllib
 import requests 
 
-#Required header for Kodi JSON-RPC calls, otherwise you'll get a 415 HTTP response code - Unsupported media type
+# Required header for Kodi JSON-RPC calls, otherwise you'll get a 415 HTTP response code - Unsupported media type
 headers = {'content-type': 'application/json'}
 
-#Host name where Kodi is running, leave as localhost if on this PC
-#Make sure "Allow control of Kodi via HTTP" is set to ON in Settings -> Services -> Webserver
+# Host name where Kodi is running, leave as localhost if on this PC
+# Make sure "Allow control of Kodi via HTTP" is set to ON in Settings -> Services -> Webserver
 host = "localhost"
 
-#User/password for "Allow control of Kodi via HTTP"
+# User/password for "Allow control of Kodi via HTTP"
 user = "kodi"
 password = "kodi"
 
-#Configured in Settings -> Services -> Webserver -> Port
+# Configured in Settings -> Services -> Webserver -> Port
 port = 8090
 
-#Base URL of the json RPC calls
-#For GET calls we append a "request" URI parameter
-#For POST calls, we add the payload as JSON the the HTTP request body
+# Base URL of the json RPC calls
+# For GET calls we append a "request" URI parameter
+# For POST calls, we add the payload as JSON the the HTTP request body
 json_rpc_url = "http://" + user + ":" + password + "@" + host + ":" + str(port) + "/jsonrpc"
 
-#Load the receiver settings
-#Set channels (5.1 channels = 8)
+# Load the receiver settings
+# Set channels (5.1 channels = 8)
 payload_channels = {'jsonrpc': '2.0', 'method': 'Settings.SetSettingValue', 'params': {'setting': 'audiooutput.channels', 'value': 8}, 'id': 1}
 url_param_channels = urllib.urlencode({'request': json.dumps(payload_channels)})
-#response = requests.get(json_rpc_url + '?' + url_param_channels, headers=headers)
+# response = requests.get(json_rpc_url + '?' + url_param_channels, headers=headers)
 response = requests.post(json_rpc_url, data=json.dumps(payload_channels), headers=headers)
-#Set passthrough = true
+# Set passthrough = true
 payload_passthrough = {'jsonrpc': '2.0', 'method': 'Settings.SetSettingValue', 'params': {'setting': 'audiooutput.passthrough', 'value': True}, 'id': 1}
 url_param_passthrough = urllib.urlencode({'request': json.dumps(payload_passthrough)})
-#response = requests.get(json_rpc_url + '?' + url_param_passthrough, headers=headers)
+# response = requests.get(json_rpc_url + '?' + url_param_passthrough, headers=headers)
 response = requests.post(json_rpc_url, data=json.dumps(payload_passthrough), headers=headers)
 
-#Show notification if loaded
+# Show notification if loaded
 if response.status_code == 200:
     payload_notification = {'jsonrpc': '2.0', 'method': 'GUI.ShowNotification', 'params': {'title': 'Audio change', 'message': 'Switching to receiver output'}, 'id': 1}
     url_param_notification = urllib.urlencode({'request': json.dumps(payload_notification)})
-	#requests.get(json_rpc_url + '?' + url_param_notification, headers=headers)
+    # requests.get(json_rpc_url + '?' + url_param_notification, headers=headers)
     requests.post(json_rpc_url, data=json.dumps(payload_notification), headers=headers)
 else:
     print response.status_code

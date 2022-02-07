dc1a461b6ba3ef4acef4075264f84fb3437be6f0
h3llrais3r <pooh_beer_1@hotmail.com>
2022-02-07 22:35:41 +0100

    Fix kodi_settings.bat

--- kodi/_scripts/kodi_settings.bat
+++ kodi/_scripts/kodi_settings.bat
@@ -1,4 +1,3 @@
 
 rem Common kodi settings for bat files
-rem set KODI_USERDATA_DIR=C:\Users\ADMIN\AppData\Roaming\Kodi\userdata
-set KODI_USERDATA_DIR=C:\Temp\testsymlinks
+set KODI_USERDATA_DIR=C:\Users\ADMIN\AppData\Roaming\Kodi\userdata

237fcfc61d52fa943fd2394227d71ebb8ae3f72f
h3llrais3r <pooh_beer_1@hotmail.com>
2022-12-08 20:47:43 +0100

    Update create_kodi_default_symlinks.bat

--- kodi/_scripts/create_kodi_default_symlinks.bat
+++ kodi/_scripts/create_kodi_default_symlinks.bat
@@ -2,6 +2,7 @@ rem Launch bat with administrative rights!
 call kodi_settings.bat
 
 echo Creating kodi default symlinks
+mklink %KODI_USERDATA_DIR%\advancedsettings.xml %~dp0..\userdata\advancedsettings.xml
 mklink %KODI_USERDATA_DIR%\sources.xml %~dp0..\userdata\sources.xml
 mklink %KODI_USERDATA_DIR%\passwords.xml %~dp0..\userdata\passwords.xml
-mklink %KODI_USERDATA_DIR%\advancedsettings.xml %~dp0..\userdata\advancedsettings.xml
+mklink %KODI_USERDATA_DIR%\wakeonlan.xml %~dp0..\userdata\wakeonlan.xml

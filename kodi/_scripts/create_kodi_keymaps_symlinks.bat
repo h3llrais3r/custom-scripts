399ab3d276caa9d933f790d17c6193256775e548
h3llrais3r <pooh_beer_1@hotmail.com>
2022-12-08 20:54:55 +0100

    Update kodi scripts

--- kodi/_scripts/create_kodi_keymaps_symlinks.bat
+++ kodi/_scripts/create_kodi_keymaps_symlinks.bat
@@ -2,7 +2,7 @@ rem Launch bat with administrative rights!
 call kodi_settings.bat
 
 echo Creating kodi keymaps symlinks
-set INPUT_DIR=userdata\keymaps
+set INPUT_DIR=keymaps
 set DEST_DIR=%KODI_USERDATA_DIR%\%INPUT_DIR%
 mkdir %DEST_DIR%
-for /f "tokens=*" %%f in ('dir /s /b /a-d "%~dp0..\%INPUT_DIR%"') do mklink "%DEST_DIR%\%%~nxf" "%%~dpnxf"
+for /f "tokens=*" %%f in ('dir /s /b /a-d "%~dp0..\userdata\%INPUT_DIR%"') do mklink "%DEST_DIR%\%%~nxf" "%%~dpnxf"
--- kodi/_scripts/create_kodi_playlists_symlinks.bat
+++ kodi/_scripts/create_kodi_playlists_symlinks.bat
@@ -2,7 +2,7 @@ rem Launch bat with administrative rights!
 call kodi_settings.bat
 
 echo Creating kodi video playlists symlinks
-set INPUT_DIR=userdata\playlists\video
+set INPUT_DIR=playlists\video
 set DEST_DIR=%KODI_USERDATA_DIR%\%INPUT_DIR%
 mkdir %DEST_DIR%
-for /f "tokens=*" %%f in ('dir /s /b /a-d "%~dp0..\%INPUT_DIR%"') do mklink "%DEST_DIR%\%%~nxf" "%%~dpnxf"
+for /f "tokens=*" %%f in ('dir /s /b /a-d "%~dp0..\userdata\%INPUT_DIR%"') do mklink "%DEST_DIR%\%%~nxf" "%%~dpnxf"

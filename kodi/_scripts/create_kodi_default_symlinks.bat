8596103c280480901f54d6243e32358d5dfcdf46
h3llrais3r <pooh_beer_1@hotmail.com>
2022-12-08 20:43:49 +0100

    Move kodi userdata scripts

--- kodi/_scripts/create_kodi_default_symlinks.bat
+++ kodi/_scripts/create_kodi_default_symlinks.bat
@@ -2,6 +2,6 @@ rem Launch bat with administrative rights!
 call kodi_settings.bat
 
 echo Creating kodi default symlinks
-mklink %KODI_USERDATA_DIR%\sources.xml %~dp0..\sources.xml
-mklink %KODI_USERDATA_DIR%\passwords.xml %~dp0..\passwords.xml
-mklink %KODI_USERDATA_DIR%\advancedsettings.xml %~dp0..\advancedsettings.xml
+mklink %KODI_USERDATA_DIR%\sources.xml %~dp0..\userdata\sources.xml
+mklink %KODI_USERDATA_DIR%\passwords.xml %~dp0..\userdata\passwords.xml
+mklink %KODI_USERDATA_DIR%\advancedsettings.xml %~dp0..\userdata\advancedsettings.xml
--- kodi/_scripts/create_kodi_library_symlinks.bat
+++ kodi/_scripts/create_kodi_library_symlinks.bat
@@ -2,13 +2,13 @@ rem Launch bat with administrative rights!
 call kodi_settings.bat
 
 echo Creating kodi movies library symlinks
-set INPUT_DIR=library\movies
+set INPUT_DIR=userdata\library\movies
 set DEST_DIR=%KODI_USERDATA_DIR%\%INPUT_DIR%
 mkdir %DEST_DIR%
 for /f "tokens=*" %%f in ('dir /s /b /a-d "%~dp0..\%INPUT_DIR%"') do mklink "%DEST_DIR%\%%~nxf" "%%~dpnxf"
 
 echo Creating kodi tvshows library symlinks
-set INPUT_DIR=library\tvshows
+set INPUT_DIR=userdata\library\tvshows
 set DEST_DIR=%KODI_USERDATA_DIR%\%INPUT_DIR%
 mkdir %DEST_DIR%
 for /f "tokens=*" %%f in ('dir /s /b /a-d "%~dp0..\%INPUT_DIR%"') do mklink "%DEST_DIR%\%%~nxf" "%%~dpnxf"
